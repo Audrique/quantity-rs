dc0fff272ab42b8a643cf543c0c44a708b845d39
Audrique Vertessen <audriquevert@gmail.com>
2026-02-28 20:16:07 +0100

    Refactor macro

---
+++ quantities-macros/src/argument_parser.rs
@@ -0,0 +1,71 @@
+use crate::error::QuantityError;
+use proc_macro2::Span;
+use syn::{
+    parse::Parse, parse::ParseStream, punctuated::Punctuated, spanned::Spanned, Path, Token,
+};
+
+enum QuantityArg {
+    Unit(Path),
+    ValueType(syn::Type),
+}
+
+impl Parse for QuantityArg {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let ident: syn::Ident = input.parse()?;
+        let _: Token![=] = input.parse()?;
+
+        match ident.to_string().as_str() {
+            "unit" => Ok(QuantityArg::Unit(input.parse()?)),
+            "value_type" => Ok(QuantityArg::ValueType(input.parse()?)),
+            other => Err(syn::Error::new(
+                ident.span(),
+                QuantityError::UnknownArgument(other.to_string()),
+            )),
+        }
+    }
+}
+
+pub struct QuantityArgs {
+    pub unit: Path,
+    pub default_type: syn::Type,
+}
+
+impl Parse for QuantityArgs {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let args: Punctuated<QuantityArg, Token![,]> =
+            input.parse_terminated(QuantityArg::parse, Token![,])?;
+
+        let mut unit = None;
+        let mut default_type = None;
+
+        for arg in args {
+            match arg {
+                QuantityArg::Unit(p) => {
+                    if unit.is_some() {
+                        return Err(syn::Error::new(
+                            p.span(),
+                            QuantityError::DuplicateArgument("unit".into()),
+                        ));
+                    }
+                    unit = Some(p);
+                }
+                QuantityArg::ValueType(t) => {
+                    if default_type.is_some() {
+                        return Err(syn::Error::new(
+                            t.span(),
+                            QuantityError::DuplicateArgument("value_type".into()),
+                        ));
+                    }
+                    default_type = Some(t);
+                }
+            }
+        }
+
+        let unit =
+            unit.ok_or_else(|| syn::Error::new(Span::call_site(), QuantityError::MissingUnit))?;
+        Ok(QuantityArgs {
+            unit,
+            default_type: default_type.unwrap_or_else(|| syn::parse_quote!(f64)),
+        })
+    }
+}
---
+++ quantities-macros/src/error.rs
@@ -0,0 +1,20 @@
+use proc_macro_crate::Error as CrateError;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum QuantityError {
+    #[error("the `#[quantity(...)]` attribute can only be used on unit structs (zero fields)")]
+    NotUnitStruct,
+
+    #[error("missing `unit` argument in `#[quantity(...)]`")]
+    MissingUnit,
+
+    #[error("unknown argument `{0}` in `#[quantity(...)]`. Valid arguments: `unit`, `value_type`")]
+    UnknownArgument(String),
+
+    #[error("duplicate `{0}` argument in `#[quantity(...)]`")]
+    DuplicateArgument(String),
+
+    #[error("`quantities` crate must be a dependency in Cargo.toml: {0}")]
+    CrateNotFound(#[from] CrateError),
+}
